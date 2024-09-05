86bf8a64aa8cc187366f4af07b224f1359798dd4
Md. Hasnain <rajon013039799@gmail.com>
2024-09-05 11:27:08 +0600

    update module base archetecture

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(morgan("tiny"));
 app.use(
   cors({
-    origin: ["https://forgotp.netlify.app"],
+    origin: ["https://tubular-pie-61a0ec.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
